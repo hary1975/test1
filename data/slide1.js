53f39232903c4d2b8945da4f3ccea939690cf2a2
hary1975 <92351854+hary1975@users.noreply.github.com>
2021-10-19 10:40:37 +0700

    Add files via upload

---
+++ data/slide1.js
@@ -0,0 +1,1 @@
+(function(){var loadHandler=window['sl_{A206D79A-E9B7-46F0-A010-F6406B3CFA64}'];loadHandler&&loadHandler(0, '<div id="spr0_5dae4c"><div id="spr1_5dae4c" class="kern slide"><img id="img0_5dae4c" src="data/img0.png" width="960px" height="540px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_5dae4c" class="kern slide"></div></div>', '{"s":[]}');})();
